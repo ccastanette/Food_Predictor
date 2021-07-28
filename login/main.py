40b0c13217b26e3bfe091da7d312c6f8972f1752
ccastanette <36716897+ccastanette@users.noreply.github.com>
2021-07-28 09:50:14 -0700

    adding routes

--- login/main.py
+++ login/main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("login.html")
 
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/diary')
+def diary():
+    return render_template("diary.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
